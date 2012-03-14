188efd6356cad4253820da24ff0437b3e5a5a9b9
Jonathan Wills <runningwild@gmail.com>
2012-03-13 22:12:05 -0700

    removed erroneous comments

--- stringz_example_test.go
+++ stringz_example_test.go
@@ -1,8 +1,3 @@
-// Copyright 2012 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// This example demonstrates decoding a JPEG image and examining its pixels.
 package stringz_test
 
 import (
